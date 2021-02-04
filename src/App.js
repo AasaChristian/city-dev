856eda368fd1df3aeface79a814044210aefb4b6
Aasa-Christian <aasachristian@yahoo.com>
2021-02-03 20:37:03 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "white",
+          height: "15%",
+        }}
+      >
+        <h1>Header</h1>
       </header>
+      <div style={{ height: "80%" }}>
+        <section
+          style={{
+            display: "flex",
+            backgroundColor: "red",
+            height: "8%",
+            borderRadius: "3%",
+          }}
+        >
+          {" "}
+          <div>
+            <h1>BOX 1</h1>
+          </div>
+        </section>
+
+        <section           style={{
+            display: "flex",
+            backgroundColor: "white",
+            height: "8%",
+            borderRadius: "3%",
+          }}>
+          <div>
+            <h1>BOX 2</h1>
+          </div>
+        </section>
+
+        <section
+          style={{ display: "flex", backgroundColor: "blue", height: "100%" }}
+        >
+          <div style={{ width: "100%" }}>
+            <h1>Main</h1>
+
+            <section
+              style={{
+                display: "flex",
+                width: "100%",
+              }}
+            >
+
+              <div >
+                <h1>Middle </h1>
+              </div>
+
+            </section>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }

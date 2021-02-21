f43ccf9eb86ae685cd7ccdb110ca3a46fc3c1abb
Aasa-Christian <aasachristian@yahoo.com>
2021-02-21 17:05:26 -0500

    boxed off areas, started banner design

--- src/App.js
+++ src/App.js
@@ -29,12 +29,14 @@ function App() {
           </div>
         </section>
 
-        <section           style={{
+        <section
+          style={{
             display: "flex",
             backgroundColor: "white",
             height: "8%",
             borderRadius: "3%",
-          }}>
+          }}
+        >
           <div>
             <h1>BOX 2</h1>
           </div>
@@ -43,22 +45,41 @@ function App() {
         <section
           style={{ display: "flex", backgroundColor: "blue", height: "100%" }}
         >
-          <div style={{ width: "100%" }}>
+          <div
+            style={{ width: "100%", display: "flex", flexDirection: "column" }}
+          >
             <h1>Main</h1>
 
             <section
               style={{
                 display: "flex",
-                width: "100%",
+                width: "99.5%",
+                border: "solid black 5px",
+                height: "70%",
               }}
             >
-
-              <div >
-                <h1>Middle </h1>
-              </div>
-
+              <section
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  width: "100%",
+                }}
+              >
+                <div>
+                  {" "}
+                  <div>
+                    <h1 style={{fontSize: '80px'}}>Welcome To </h1>
+                  </div>
+                  <div>
+                    <h1 style={{fontSize: '100px'}}>Ace City </h1>
+                  </div>
+                </div>
+              </section>
             </section>
           </div>
+        </section>
+        <section>
+          <h1>Footer</h1>
         </section>
       </div>
     </div>

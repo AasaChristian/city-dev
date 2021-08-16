60928adb7f23ca88047c334a0dddbcf2197e63aa
Aasa-Christian <aasachristian@yahoo.com>
2021-08-16 19:21:41 -0400

    created header

--- src/components/HeaderButtons.js
+++ src/components/HeaderButtons.js
@@ -1,8 +1,14 @@
+import React, { useState } from "react";
+
 function HeaderButtons(props){
-const {buttonTitle} = props
+const {buttonTitle, flipPop} = props
+
+const onClick = (e) => {
+  flipPop()
+}
 
     return(
-        <div style={{border: 'solid black 2px', display:'flex', flexDirection: 'column', justifyContent: 'center', borderRadius: "15%",}}>
+        <div onClick={onClick} style={{border: 'solid black 2px', display:'flex', flexDirection: 'column', justifyContent: 'center', borderRadius: "15%",}}>
         <h1
         
         > {buttonTitle} </h1>
